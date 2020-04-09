4b4763cf0a773c6c98bf4908c32feec4d96036dc
Daniel Dugovic <dandydand@gmail.com>
2020-04-09 05:00:48 -0500

    Remove seconds from time format

--- test/test.js
+++ test/test.js
@@ -2,17 +2,26 @@ var assert = require('assert');
 var format = require('../format-seconds');
 describe('Commands', function() {
   describe('formatSeconds()', function() {
-    it('should format 1 second', function() {
-      assert.equal("1 second", format.formatSeconds(1));
-    });
     it('should format 1 minute', function() {
       assert.equal("1 minute", format.formatSeconds(60));
+    });
+    it('should format 2 minutes', function() {
+      assert.equal("2 minutes", format.formatSeconds(120));
     });
     it('should format 1 hour', function() {
       assert.equal("1 hour", format.formatSeconds(3600));
     });
+    it('should format 2 hours', function() {
+      assert.equal("2 hours", format.formatSeconds(7200));
+    });
     it('should format 1 day', function() {
       assert.equal("1 day", format.formatSeconds(86400));
+    });
+    it('should format 2 days', function() {
+      assert.equal("2 days", format.formatSeconds(172800));
+    });
+    it('should format 1 day, 1 hour, 1 minute', function() {
+      assert.equal("1 day, 1 hour, 1 minute", format.formatSeconds(90061));
     });
   });
 });
